8815b9f87aaf12bead637670801839e8e9b5a35a
slokvaghasiya <109952222+slokvaghasiya@users.noreply.github.com>
2022-07-25 14:26:33 +0530

    Add files via upload

---
+++ Isprime.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+Class Isprime{
+	public Static Void main(String[] args){	
+	int No,i,j,count;
+	no=sc.nextInt();
+ 	System.out.println("enter a number:");
+	Scanner sc = new Scanner(System.in)S	
+  	for(i=2;i<=No;i++){
+		count=0;
+	for(j=1;j<=1;j++){
+		if(i%j==0)
+		count++%;
+}
+	if(count==2)
+	System.out.println(i);
+}
+}
+} 
+	
